77bfa60703457870062d583f29a37d2e2746b35c
Patrick Marabeas <patrick@pulse-dev.com.au>
2013-06-15 13:17:51 +1000

    Update stickyNav.js
    Added a reset method which performs what doing the combo of destroy and init was supposed to do but failed

--- stickyNav.js
+++ stickyNav.js
@@ -3,11 +3,11 @@
 	$.stickyNav = function(element, config) {
 	
 		var $element = $(element),
-             element = element;
+            	 element = element;
 		var plugin = this;
 		
 		var defaults = {
-
+			offset: 0
 		};
 		var config = $.extend(defaults, config);
 		
@@ -17,8 +17,8 @@
 		var calc;
 		
 		plugin.init = function() {
-
-			stickyPoint = $element.offset().top;
+			
+			stickyPoint = $element.offset().top - config.offset;
 			stickyHeight = $element.outerHeight(); //returns an integer
 			nextPadding = parseInt($element.next().css('padding-top')); //.css() returns px, need an integer
 			calc = stickyHeight + nextPadding;
@@ -42,31 +42,28 @@
 			console.log('destroyed');
 		};
 		
+		plugin.reset = function() {
+			$element.removeClass('fixed').next().removeClass('padding').css('padding-top',(nextPadding));
+			
+			stickyPoint = $element.offset().top - config.offset;
+			stickyHeight = $element.outerHeight(); //returns an integer
+			nextPadding = parseInt($element.next().css('padding-top')); //.css() returns px, need an integer
+			calc = stickyHeight + nextPadding;
+			
+			if ($(window).scrollTop() > stickyPoint){
+				$element.addClass('fixed').next().addClass('padding').css('padding-top',(calc));
+			}
+			else {
+				$element.removeClass('fixed').next().removeClass('padding').css('padding-top',(nextPadding));
+			}
+		};
+		
 		$(window).resize(function() {
-			plugin.destroy();
 			plugin.init();
 		});
 
 		plugin.init();
 	
 	};
-	
-	$.fn.stickyNav = function(config) {
-
-        return this.each(function() {
-            if (undefined == $(this).data('stickyNav')) {
-                var plugin = new $.stickyNav(this, config);
-                $(this).data('stickyNav', plugin);
-            }
-        });
-
-    }
-	
-	$(window).on('load', function () {
-		if ($("body").attr('data-stickynavtarget')) {
-			var target = $('body').data('stickynavtarget')
-			$(target).stickyNav();
-		}
-	});
 	
 })(jQuery)
